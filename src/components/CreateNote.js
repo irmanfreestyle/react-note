cc55b71e328c529c304373817812bb330a405e7d
irmanfreestyle <irmancoder77@gmail.com>
2021-03-09 21:38:59 +0700

    finish create multiple select labels feature

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -2,11 +2,12 @@ import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {showCreate, createNote} from '../actions/noteActions';
 import CreateModal from './slots/ModalSlot';
-import Alert from './slots/ModalSlot';
+import SelectLabel from './slots/ModalSlot';
 import moment from 'moment';
 import {uid} from 'uid';
 
 export default function CreateNote () {
+  let [showSelectLabelModal, setShowSelectLabelModal] = useState(false);
   let [title, setTitle] = useState('');
   let [labels, setLabels] = useState('');
   let [content, setContent] = useState('');
@@ -15,7 +16,7 @@ export default function CreateNote () {
   let [uidNote, setUid] = useState('');
   let isEdit = useSelector(state => state.notes.isEdit);
   let dispatch = useDispatch();
-  let createdLabels = useSelector(state => state.notes.labels)
+  let createdLabels = useSelector(state => state.notes.labels).map(item => ({...item, selected: false}))
 
   let addNote = () => {
     let payload = {
@@ -59,8 +60,16 @@ export default function CreateNote () {
     }
   };
 
-  function onSelectLabels () {
-    console.log(createdLabels)
+  function onSelectLabels (values) {
+    let prevValues = values.split(',')
+
+    createdLabels.forEach(item => {
+      if (prevValues.includes(item.title)) {
+        item.selected = true
+      }
+    })
+
+    setShowSelectLabelModal(true)
   }
 
   useEffect(() => {
@@ -81,42 +90,101 @@ export default function CreateNote () {
     )
   }
 
+  let ModalSelectLabel = () => {
+    function chooseLabel () {
+      let selectedLabels = ''
+
+      createdLabels.forEach(item => {
+        if (item.selected) {
+          selectedLabels += `${item.title}, `
+        }
+      })
+
+      setLabels(selectedLabels.slice(0, selectedLabels.length - 2))
+      setShowSelectLabelModal(false)
+    }
+
+    function handleSelectLabel (value) {
+      createdLabels.forEach(item => {
+        if (item.title === value) {
+          item.selected = !item.selected
+        }
+      })
+    }
+
+    return !showSelectLabelModal ? '' : (
+      <SelectLabel
+        header={<h3 className="text-center">Select note labels</h3>}
+        content={
+          createdLabels.map(item => {
+            return (
+              <div
+                key={item.title}
+              >
+                <input id={item.title} type="checkbox" selected={item.selected} value={item.title} onChange={() => handleSelectLabel(item.title)}></input>
+                <label htmlFor={item.title}>
+                  { item.title }
+                </label>
+              </div>
+            )
+          })
+        }
+        actions={
+          <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <button className="btn-danger" onClick={() => setShowSelectLabelModal(false)}>Close</button> &nbsp;
+            <button
+              className="btn-main"
+              onClick={chooseLabel}
+            >
+              Confirm
+            </button>
+          </div>
+        }
+      />
+    )
+  }
+
   return (
-    <CreateModal
-      header={<h3 className="text-center">{isEdit ? 'Edit' : 'Create a'} note</h3>}
-      content={
-        <>
-        <input
-          type="text"
-          placeholder="Title"
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-        />
-        <input
-          type="text"
-          placeholder="Label"
-          onChange={(e) => setLabels(e.target.value)}
-          value={labels}
-          onClick={onSelectLabels}
-        />
-        <textarea
-          rows="5"
-          placeholder="Write something here..."
-          onChange={(e) => setContent(e.target.value)}
-          value={content}
-        ></textarea>
-        <input
-          type="checkbox"
-          id="pin"
-          onChange={(e) => setPinned(e.target.checked)}
-          checked={pinned}
-        />
-        <label htmlFor="pin">
-          Pin this note
-        </label>
-        </>
-      }
-      actions={<Action />}
-    />
+    <>
+      <CreateModal
+        header={<h3 className="text-center">{isEdit ? 'Edit' : 'Create a'} note</h3>}
+        content={
+          <>
+          <input
+            type="text"
+            placeholder="Title"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+          <input
+            type="text"
+            placeholder="Label"
+            onChange={(e) => setLabels(e.target.value)}
+            value={labels}
+            onClick={() => onSelectLabels(labels)}
+            readOnly
+          />
+          <textarea
+            rows="5"
+            placeholder="Write something here..."
+            onChange={(e) => setContent(e.target.value)}
+            value={content}
+          ></textarea>
+          <input
+            type="checkbox"
+            id="pin"
+            onChange={(e) => setPinned(e.target.checked)}
+            checked={pinned}
+          />
+          <label htmlFor="pin">
+            Pin this note
+          </label>
+          </>
+        }
+        actions={<Action />}
+      />
+
+      <ModalSelectLabel />
+    </>
   )
 }
